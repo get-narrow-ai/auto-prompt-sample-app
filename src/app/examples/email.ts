a15ceaf40576b99163fcda1786648f7ecc901ecf
aychtang <aychtang+gh@gmail.com>
2024-03-19 15:14:37 +0000

    feat: Add "cold email" sample option to the app

---
+++ src/app/examples/email.ts
@@ -0,0 +1,31 @@
+export default {
+  prompt: `Write me a cold sales email`,
+  context: `## Prospect Name:
+
+Howard Tang
+
+## Prospect Email:
+
+howard@codethread.ai
+
+## Use cases
+If they are LLM app developers - means they need to write a lot of prompts for our LLMs. We can help automate the writing of feature and eval prompts
+If they are workflow automation providers - means their users need to write prompts and they can use our user voice learning
+
+## Prospect Use Case:
+
+CodeThread is developing an LLM application.
+
+## Prospect website content:
+
+A new way to centralize and share code knowledge CodeThread's AI powered suite helps you maintain your internal code knowledge automatically - so you can focus on coding. Work Email Make writing and maintaining docs a background process Easily create docs before pushing your code Get suggestions when docs need updating Share your code knowledge effortlessly Instantly match questions to the right people Route questions to the right person Async friendly communication Never lose context of your conversations Organize your codebase and centralize information Visualize your services and boundaries Track your tech debt and migrations Integrate your tags with external tools Let us take care of your Onboarding Documentation Q&A Handovers Get early access We’re a developer success platform that gives software teams tools for onboarding, collaboration and knowledge management. Work Email info@codethread.ai
+
+## Our product:
+* Name: Narrow - getnarrow.ai
+* We offer auto prompt engineering or "prompting as a service" for working with LLMs
+* Using our automatic prompt engineering, we can also learn the "user voice" of your users to help them automate the writing of generative content like emails or chat responses
+
+## Their use case:
+* AI developers spend a lot of time writing prompts for their LLM features
+* LLM features are also hard to test and debug, using eval prompt generation, we can help monitor them more effectively`,
+};
